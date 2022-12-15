e69d6923d6dc8f3e028731230373f0952af9f621
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2022-12-15 16:23:57 -0500

    Modules creation (heroes-auth)

--- src/app/heros/heros.module.ts
+++ src/app/heros/heros.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CreateComponent } from './pages/create/create.component';
+import { HeroComponent } from './pages/hero/hero.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/list/list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CreateComponent,
+    HeroComponent,
+    HomeComponent,
+    ListComponent
+  ],
   imports: [
     CommonModule
   ]
