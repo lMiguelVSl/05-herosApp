50cbbc49dca1844b0aea56c911bd48e0a449b024
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2022-12-15 16:26:35 -0500

    error-module showing as module into HTML main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
     BrowserModule
