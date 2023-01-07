e2d3c8efb018da493edd0b5f58872d967fb1a87b
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-01-07 13:36:29 -0500

    Getting hero with rxjs operators

--- src/app/heros/pages/hero/hero.component.ts
+++ src/app/heros/pages/hero/hero.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable, Subscription } from 'rxjs';
+import { Subscription, switchMap } from 'rxjs';
+
 import { Hero } from '../../interfaces/heroes.interface';
 import { HerosService } from '../../services/heros.service';
 
@@ -13,17 +14,17 @@ export class HeroComponent implements OnInit, OnDestroy{
   @Input() heroId: string = ''; 
 
   heroIdRoute!: Subscription; 
-  hero: Observable<Hero> = new Observable;
+  hero!: Hero;
 
-  constructor(private activatedRoute: ActivatedRoute, private heroService: HerosService) {
-        
-  }
+  constructor(private activatedRoute: ActivatedRoute, private heroService: HerosService) {}
 
   ngOnInit(): void {
     this.heroIdRoute = this.activatedRoute.params
-    .subscribe(({id}) => {
-      this.hero = this.heroService.getHeroById(id);
-      console.log(this.hero);
+    .pipe(
+      switchMap(({id}) => this.heroService.getHeroById(id))
+    )
+    .subscribe(hero => {
+      this.hero = hero;
     });
   }
 
