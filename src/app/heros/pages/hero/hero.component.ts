68b5b480643159f0e9e72a0e8f18823ef8aa06f7
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-01-07 13:23:15 -0500

    using rxjs to get the obj from the service

--- src/app/heros/pages/hero/hero.component.ts
+++ src/app/heros/pages/hero/hero.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { Hero } from '../../interfaces/heroes.interface';
+import { HerosService } from '../../services/heros.service';
 
 @Component({
   selector: 'app-hero',
@@ -10,15 +12,19 @@ export class HeroComponent implements OnInit, OnDestroy{
   
   @Input() heroId: string = ''; 
 
-  heroIdRoute: Subscription | undefined; 
+  heroIdRoute!: Subscription; 
+  hero: Observable<Hero> = new Observable;
 
-  constructor(private activatedRoute: ActivatedRoute) {
+  constructor(private activatedRoute: ActivatedRoute, private heroService: HerosService) {
         
   }
 
   ngOnInit(): void {
     this.heroIdRoute = this.activatedRoute.params
-    .subscribe(({id}) => console.log(id));
+    .subscribe(({id}) => {
+      this.hero = this.heroService.getHeroById(id);
+      console.log(this.hero);
+    });
   }
 
   ngOnDestroy(): void {
--- src/app/heros/services/heros.service.ts
+++ src/app/heros/services/heros.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { Hero } from '../interfaces/heroes.interface';
 
 @Injectable({
@@ -11,7 +12,10 @@ export class HerosService {
     private http: HttpClient
   ) { }
 
-  getHeroes() {
+  getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>('http://localhost:3000/heroes');
+  }
+  getHeroById(id: string): Observable<Hero> {
+    return this.http.get<Hero>(`http://localhost:3000/heroes/${id}`);
   }
 }
