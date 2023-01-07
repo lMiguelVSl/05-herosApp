5abe9d2a46fb5a34ccd34bcfc325c9bfcff11f9d
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-01-07 12:57:08 -0500

    Subscription manage

--- src/app/heros/pages/hero/hero.component.ts
+++ src/app/heros/pages/hero/hero.component.ts
@@ -1,15 +1,28 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-hero',
   templateUrl: './hero.component.html'
 })
-export class HeroComponent implements OnInit{
+export class HeroComponent implements OnInit, OnDestroy{
   
   @Input() heroId: string = ''; 
 
+  heroIdRoute: Subscription | undefined; 
+
+  constructor(private activatedRoute: ActivatedRoute) {
+        
+  }
+
   ngOnInit(): void {
-    console.log();
+    this.heroIdRoute = this.activatedRoute.params
+    .subscribe(({id}) => console.log(id));
+  }
+
+  ngOnDestroy(): void {
+  if (this.heroIdRoute) this.heroIdRoute.unsubscribe();
   }
 
 }
