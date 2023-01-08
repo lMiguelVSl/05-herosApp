d4f061b5573595fbeb441566ce114e27e5b4c867
Miguel <78430957+lMiguelVSl@users.noreply.github.com>
2023-01-08 16:42:14 -0500

    hero routing

--- src/app/heros/pages/hero/hero.component.ts
+++ src/app/heros/pages/hero/hero.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription, switchMap } from 'rxjs';
 
 import { Hero } from '../../interfaces/heroes.interface';
@@ -7,7 +7,13 @@ import { HerosService } from '../../services/heros.service';
 
 @Component({
   selector: 'app-hero',
-  templateUrl: './hero.component.html'
+  templateUrl: './hero.component.html',
+  styles:[`
+    img{
+      width:100%;
+      border-radius: 5px;
+    }
+  `]
 })
 export class HeroComponent implements OnInit, OnDestroy{
   
@@ -16,7 +22,10 @@ export class HeroComponent implements OnInit, OnDestroy{
   heroIdRoute!: Subscription; 
   hero!: Hero;
 
-  constructor(private activatedRoute: ActivatedRoute, private heroService: HerosService) {}
+  constructor(
+    private activatedRoute: ActivatedRoute, 
+    private heroService: HerosService,
+    private router: Router) {}
 
   ngOnInit(): void {
     this.heroIdRoute = this.activatedRoute.params
@@ -30,6 +39,10 @@ export class HeroComponent implements OnInit, OnDestroy{
 
   ngOnDestroy(): void {
   if (this.heroIdRoute) this.heroIdRoute.unsubscribe();
+  }
+
+  regresar(){
+  this.router.navigate(['/heros/list'])
   }
 
 }
